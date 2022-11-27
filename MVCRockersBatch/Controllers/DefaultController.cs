55120c17b892d113103d65ecbfd814598860f41c
abhishekkatta <abhishekk@samisen.io>
2022-11-27 23:37:48 +0530

    SecondInitial

--- MVCRockersBatch/Controllers/DefaultController.cs
+++ MVCRockersBatch/Controllers/DefaultController.cs
@@ -71,5 +71,85 @@ namespace MVCRockersBatch.Controllers
             ViewBag.empInfo = listEmp;
             return View();
         }
+
+        public ActionResult SendData3()
+        {
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Raj";
+            emp.EmpSalary = 30000;
+
+            
+            return View(emp);
+        }
+
+        public ActionResult SendData4()
+        {
+            List<EmployeeModel> listEmp = new List<EmployeeModel>();
+
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Raj";
+            emp.EmpSalary = 30000;
+
+            EmployeeModel emp1 = new EmployeeModel();
+            emp1.EmpId = 2;
+            emp1.EmpName = "Rani";
+            emp1.EmpSalary = 31000;
+
+            EmployeeModel emp2 = new EmployeeModel();
+            emp2.EmpId = 3;
+            emp2.EmpName = "Raji";
+            emp2.EmpSalary = 32000;
+
+            listEmp.Add(emp);
+            listEmp.Add(emp1);
+            listEmp.Add(emp2);
+
+            
+            return View(listEmp);
+        }
+
+        public ActionResult SendData5()
+        {
+            List<EmployeeModel> listEmp = new List<EmployeeModel>();
+
+            EmployeeModel emp = new EmployeeModel();
+            emp.EmpId = 1;
+            emp.EmpName = "Raj";
+            emp.EmpSalary = 30000;
+
+            EmployeeModel emp1 = new EmployeeModel();
+            emp1.EmpId = 2;
+            emp1.EmpName = "Rani";
+            emp1.EmpSalary = 31000;
+
+            EmployeeModel emp2 = new EmployeeModel();
+            emp2.EmpId = 3;
+            emp2.EmpName = "Raji";
+            emp2.EmpSalary = 32000;
+
+            listEmp.Add(emp);
+            listEmp.Add(emp1);
+            listEmp.Add(emp2);
+
+            List<DepartmentModel> listdept = new List<DepartmentModel>();
+            DepartmentModel dept = new DepartmentModel();
+            dept.DeptId = 1;
+            dept.DeptName = "First";
+
+            DepartmentModel dept1 = new DepartmentModel();
+            dept1.DeptId = 2;
+            dept1.DeptName = "Second";
+
+            listdept.Add(dept);
+            listdept.Add(dept1);
+
+            EmployeeDepartment empdept = new EmployeeDepartment();
+            empdept.emp = listEmp;
+            empdept.dept = listdept;
+
+            return View(empdept);
+        }
     }
 }
